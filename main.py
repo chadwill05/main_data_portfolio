b738c9e5dc42d2c8c804ac070e27f9b0a37c206d
Chad <chadwill05@gmail.com>
2023-11-28 22:55:25 -0500

    not working but committing. V2

--- main.py
+++ main.py
@@ -9,20 +9,20 @@ from functools import wraps
 from werkzeug.security import generate_password_hash, check_password_hash
 from sqlalchemy.orm import relationship
 
-import os
-
+#import os
+#
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY')
-ckeditor = CKEditor(app)
-Bootstrap5(app)
-
-
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI", "sqlite:///projects.db")
-db = SQLAlchemy()
-db.init_app(app)
-
-with app.app_context():
-    db.create_all()
+#app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY')
+#ckeditor = CKEditor(app)
+#Bootstrap5(app)
+#
+#
+#app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI", "sqlite:///projects.db")
+#db = SQLAlchemy()
+#db.init_app(app)
+#
+#with app.app_context():
+#    db.create_all()
 
 
 @app.route('/')
@@ -30,29 +30,29 @@ def home():
     return render_template('index.html')
 
 
-@app.route('/resume')
-def resume():
-    return render_template('resume.html')
+#app.route('/resume')
+#ef resume():
+#   return render_template('resume.html')
 
-@app.route('/projects')
-def projects():
-    return render_template('projects.html')
+#app.route('/projects')
+#ef projects():
+#   return render_template('projects.html')
 
-@app.route('/contact')
-def contact():
-    return render_template('contact.html')
+#app.route('/contact')
+#ef contact():
+#   return render_template('contact.html')
 
-@app.route('/tableau-projects')
-def tableau_projects():
-    return render_template('tableau-projects.html')
+#app.route('/tableau-projects')
+#ef tableau_projects():
+#   return render_template('tableau-projects.html')
 
-@app.route('/data-projects')
-def data_projects():
-    return render_template('data-projects.html')
+#app.route('/data-projects')
+#ef data_projects():
+#   return render_template('data-projects.html')
 
-@app.route('/python-projects')
-def python_projects():
-    return render_template('python-projects.html')
+#app.route('/python-projects')
+#ef python_projects():
+#   return render_template('python-projects.html')
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
