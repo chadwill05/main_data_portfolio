c0f838da2b825ed392a379e158f46eec86ff0f99
Chad <chadwill05@gmail.com>
2023-11-28 22:37:35 -0500

    not working but committing. V2

--- main.py
+++ main.py
@@ -1,16 +1,58 @@
-# This is a sample Python script.
+from datetime import date
+from flask import Flask, abort, render_template, redirect, url_for, flash, request
+from flask_bootstrap import Bootstrap5
+from flask_ckeditor import CKEditor
+from flask_gravatar import Gravatar
+from flask_login import UserMixin, login_user, LoginManager, current_user, logout_user
+from flask_sqlalchemy import SQLAlchemy
+from functools import wraps
+from werkzeug.security import generate_password_hash, check_password_hash
+from sqlalchemy.orm import relationship
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import os
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY')
+ckeditor = CKEditor(app)
+Bootstrap5(app)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI", "sqlite:///projects.db")
+db = SQLAlchemy()
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
 
 
-# Press the green button in the gutter to run the script.
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/resume')
+def resume():
+    return render_template('resume.html')
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/tableau-projects')
+def tableau_projects():
+    return render_template('tableau-projects.html')
+
+@app.route('/data-projects')
+def data_projects():
+    return render_template('data-projects.html')
+
+@app.route('/python-projects')
+def python_projects():
+    return render_template('python-projects.html')
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    app.run(debug=True, port=5001)
